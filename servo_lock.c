96ddb45b27df046154c6aa0ffad67def0ebbb382
jeonjw25 <jeonjw25@naver.com>
2020-06-13 17:30:07 +0900

    서보모터 구동 소스 및 php파일

---
+++ servo_lock.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <wiringPi.h>
+#include <softPwm.h>
+
+#define SERVO_PIN 1
+
+int main() {
+	if(wiringPiSetup() == -1)
+		return 1;
+	
+	pinMode(SERVO_PIN, PWM_OUTPUT);
+	pwmSetMode(PWM_MODE_MS);
+	pwmSetClock(384);
+	pwmSetRange(1000);
+	pwmWrite(SERVO_PIN, 120);
+	printf("1");
+}
