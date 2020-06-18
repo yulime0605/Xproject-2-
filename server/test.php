6c5d8fabbe488621896a9758e2aa425b15a2608c
jeonjw25 <54730375+jeonjw25@users.noreply.github.com>
2020-06-18 21:05:48 +0900

    php에서 라즈베리파이 서보모터구동

--- server/test.php
+++ server/test.php
@@ -6,46 +6,24 @@
     $userNumber = $_POST["userNumber"];
     $bikeNumber = $_POST["bikeNumber"];
 
-    // echo "$bikeNumber";
-    // $response = array();
-    // $response["bikeNumber"] = $bikeNumber;
-    // $response["success"] = true;
-    // echo json_encode($response);
-    // if(empty($userName)) {
-    //     $errMSG = "이름x";
-    // }
-    // if(empty($userNumber)) {
-    //     $errMSG = "전화번호x";
-    // }
-    // if(empty($bikeNumber)) {
-    //     $errMSG = "자전거번호x";
-    // }
-
-    // if(!$con) {
-    //     $errMSG = "X";
-    // }
-
     $statement = mysqli_prepare($con, "SELECT Bike_Number FROM USING_BIKE WHERE Bike_Number = ?");
     mysqli_stmt_bind_param($statement, "s", $bikeNumber);
     mysqli_stmt_execute($statement);
     
 
     mysqli_stmt_store_result($statement);
-    mysqli_stmt_bind_result($statement);
-
+    mysqli_stmt_bind_result($statement, $bikeNumber);
     $response = array();
     $response["success"] = false;
- 
+   
+   
     while(!(mysqli_stmt_fetch($statement))) {
+        $bikeNumber = $_POST["bikeNumber"];
         $statement = mysqli_prepare($con, "INSERT INTO USING_BIKE VALUES (?,?,?)");
-        mysqli_stmt_bind_param($statement, "sss", $bikeNumber, $userName, $userNumber);
+        mysqli_stmt_bind_param($statement, "sss", strval($bikeNumber), $userName, strval($userNumber));
         mysqli_stmt_execute($statement);
         $response["success"] = true;
-        break;
-    }
-    
-   
-        //$response["bikeNumber"] = $bikeNumber;
+        $_SERVER['http://10.10.1.73/Boo.php?unlock']
         // if(isset($_GET['lock'])) {
         //     $ret = shell_exec('/home/pi/servo_lock 2>&1');
         //     echo $ret;
@@ -53,6 +31,11 @@
         // else if(isset($_GET['unlock'])) {
         //     $ret = shell_exec('/home/pi/servo_unlock 2>&1');
         //     echo $ret;
+        break;
+    }
+    
+   
+        //$response["bikeNumber"] = $bikeNumber;
         // }
     // }
     echo json_encode($response);
